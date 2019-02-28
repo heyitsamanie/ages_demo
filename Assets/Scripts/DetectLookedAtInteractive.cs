603b0c1fd11e00f8c068f262572b9825a3ebb125
heyitsamanie <amanie.safadi@loop.colum.edu>
2019-02-28 14:25:53 -0600

    Interactive Objects Video #2

--- Assets/Scripts/DetectLookedAtInteractive.cs
+++ Assets/Scripts/DetectLookedAtInteractive.cs
@@ -18,20 +18,35 @@ public class DetectLookedAtInteractive : MonoBehaviour
     [SerializeField]
     private float maxRange = 5.0f;
 
+    public IInteractive LookedAtInteractive
+    {
+        get { return lookedAtInteractive; }
+        private set { lookedAtInteractive = value; }
+    }
+
+    private IInteractive lookedAtInteractive;
+
     private Vector3 raycastDirection;
 
     private void FixedUpdate()
     {
         Debug.DrawRay(raycastOrigin.position, raycastOrigin.forward * maxRange, Color.red);
         RaycastHit hitInfo;
+        bool objectWasDetected = Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hitInfo, maxRange);
 
-        bool objectWasDetected = Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hitInfo, maxRange); 
+        IInteractive interactive = null;
+
+        LookedAtInteractive = interactive;
 
         if (objectWasDetected)
         {
-            Debug.Log($"Player is looking at: {hitInfo.collider.gameObject.name}");
+            //Debug.Log($"Player is looking at: {hitInfo.collider.gameObject.name}");
+            interactive = hitInfo.collider.gameObject.GetComponent<IInteractive>();
         }
-       
+
+        if (interactive != null)
+            lookedAtInteractive = interactive;
+           
     }
 
 }
---
+++ Assets/Scripts/InteractWithLookedAt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Detects when the player presses the interact button when looking at an IInteractive,
+/// and then calls that IInteractive's InteractWith method
+/// </summary>
+
+public class InteractWithLookedAt : MonoBehaviour
+{
+    [SerializeField]
+    private DetectLookedAtInteractive detectLookedAtInteractive;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && detectLookedAtInteractive.LookedAtInteractive != null)
+        {
+            Debug.Log("Player pressed the interact button");
+            detectLookedAtInteractive.LookedAtInteractive.InteractWith();
+        }
+    }
+}
