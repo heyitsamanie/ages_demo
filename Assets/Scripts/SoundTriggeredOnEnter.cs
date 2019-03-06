301467fc9d82d3541fb0bdd8aef93c5fb26db797
heyitsamanie <amanie.safadi@loop.colum.edu>
2019-03-05 18:38:13 -0600

    radio and record player asset plus sounds

---
+++ Assets/Scripts/SoundTriggeredOnEnter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTriggeredOnEnter : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CharacterController player = other.GetComponent<CharacterController>();
+        audioSource.Play();
+    }
+}
