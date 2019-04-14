ac9cb4daa248f96b3dea990324aa3e3526279b32
heyitsamanie <amanie.safadi@loop.colum.edu>
2019-04-14 15:41:27 -0500

    Locked Door & Inventory Objects

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,6 +5,24 @@ using UnityEngine;
 [RequireComponent (typeof (Animator))]
 public class Door : InteractiveObject
 {
+    [Tooltip("Check this box to lock the door.")]
+    [SerializeField]
+    private bool isLocked;
+
+    [Tooltip("The text displayed when the player looks at the door when it's locked.")]
+    [SerializeField]
+    private string lockedDisplayText = "Locked";
+
+    [Tooltip("Play this AudioClip when the player interacts with a locked door without owning the key.")]
+    [SerializeField]
+    private AudioClip lockedAudioClip;
+
+    [Tooltip("Play this AudioClip when the player opens the door.")]
+    [SerializeField]
+    private AudioClip openAudioClip;
+
+    public override string DisplayText => isLocked ? lockedDisplayText : base.DisplayText;
+
     private Animator animator;
     private bool isOpen = false;
     private int shouldOpenAnimParameter = Animator.StringToHash(nameof(shouldOpenAnimParameter));
@@ -28,17 +46,27 @@ public class Door : InteractiveObject
     {
         if (!isOpen)
         {
-            base.InteractWith();
-            animator.SetBool(shouldOpenAnimParameter, true);
-            displayText = string.Empty;
-            isOpen = true;
+            if (!isLocked)
+            {
+                audioSource.clip = openAudioClip;
+                animator.SetBool(shouldOpenAnimParameter, true);
+                displayText = string.Empty;
+                isOpen = true;
+            }
+            else // If the door is locked ...
+            {
+                audioSource.clip = lockedAudioClip;
+            }
+
+            base.InteractWith(); // This plays a sound effect!
         }
-        else
-        {
-            base.InteractWith();
-            animator.SetBool(shouldCloseAnimParameter, true);
-            displayText = string.Empty;
-            isOpen = false;
-        }
+
+        //else
+        //{
+        //    base.InteractWith();
+        //    animator.SetBool(shouldCloseAnimParameter, true);
+        //    displayText = string.Empty;
+        //    isOpen = false;
+        //}
     }
 }
--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof (AudioSource))]
 public class InteractiveObject : MonoBehaviour, IInteractive
 {
+    [Tooltip("text that will display in the UI when the player look at this object in the world.")]
     [SerializeField]
     protected string displayText = nameof (InteractiveObject);
 
-    public string DisplayText => displayText;
-    private AudioSource audioSource;
+    public virtual string DisplayText => displayText;
+    protected AudioSource audioSource;
 
     protected virtual void Awake()
     {
